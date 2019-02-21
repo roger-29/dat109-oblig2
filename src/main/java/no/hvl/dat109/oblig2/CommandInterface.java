1c3a21921f096c2804b649359dc892443002fa6b
Sondre Gjellestad <sondre.gjellestad@hotmail.com>
2019-02-22 00:29:37 +0100

    :tada: Initialized Gradle Java app

---
+++ src/main/java/no/hvl/dat109/oblig2/CommandInterface.java
@@ -0,0 +1,15 @@
+package no.hvl.dat109.oblig2;
+
+class CommandInterface {
+
+	public static void printInfo(String info) {
+
+		return;
+	}
+
+	public static String doPromptString(String question) {
+
+
+		return "";
+	}
+}
